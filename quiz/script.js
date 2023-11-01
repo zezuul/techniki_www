8672e3cd85ee1a11ba5575172fcece5b89e11307
zezuul <zezula.julia@gmail.com>
2023-11-01 14:25:07 +0100

    Admin login

--- quiz/script.js
+++ quiz/script.js
@@ -20,71 +20,71 @@ const questions = [
     {
         question: "Co nie jest metodą protokołu HTTP?",
         options: [
-        { text: "HEAD", correct: false },
-        { text: "GET", correct: false },
-        { text: "DUPLICATE", correct: true },
-        { text: "POST", correct: false }
+            { text: "HEAD", correct: false },
+            { text: "GET", correct: false },
+            { text: "DUPLICATE", correct: true },
+            { text: "POST", correct: false }
         ]
     },
     {
         question: "HTTPS - zaznacz fałszywe",
         options: [
-        { text: "Wersja szyfrowana protokołu HTTP oparta o protokół szyfrujący SSL", correct: false },
-        { text: "Wymiana danych w tym protokole zapobiega przechwyceniu oraz zmianie danych w trakcie komunikacji klient-serwer", correct: false },
-        { text: "SSL działa warstwę niżej w modelu OSI od samego HTTPS ", correct: true },
-        { text: "Domyślnym portem na których serwer nasłuchuje żądań HTTPS to 443", correct: false }
+            { text: "Wersja szyfrowana protokołu HTTP oparta o protokół szyfrujący SSL", correct: false },
+            { text: "Wymiana danych w tym protokole zapobiega przechwyceniu oraz zmianie danych w trakcie komunikacji klient-serwer", correct: false },
+            { text: "SSL działa warstwę niżej w modelu OSI od samego HTTPS ", correct: true },
+            { text: "Domyślnym portem na których serwer nasłuchuje żądań HTTPS to 443", correct: false }
         ]
     },
     {
         question: "Kod stanu serwera jest podawana w nagłówku HTTP/HTTPS w postaci liczby",
         options: [
-        { text: "jednocyfrowej", correct: false },
-        { text: "dwucyfrowej", correct: false },
-        { text: "trzycyfrowej", correct: true },
-        { text: "czterocyfrowej", correct: false }
+            { text: "jednocyfrowej", correct: false },
+            { text: "dwucyfrowej", correct: false },
+            { text: "trzycyfrowej", correct: true },
+            { text: "czterocyfrowej", correct: false }
         ]
     },
     {
         question: "Co oznacza kod 404?",
         options: [
-        { text: "wewnętrzny błąd serwera", correct: false },
-        { text: "konflikt", correct: false },
-        { text: "Nie znaleziono", correct: true },
-        { text: "niepoprawne zapytanie", correct: false }
+            { text: "wewnętrzny błąd serwera", correct: false },
+            { text: "konflikt", correct: false },
+            { text: "Nie znaleziono", correct: true },
+            { text: "niepoprawne zapytanie", correct: false }
         ]
     },
     {
         question: "Statyczna strona WWW - zaznacz fałszywe",
         options: [
-        { text: "użytkownik nie posiadający wiedzy na temat struktury strony nie jest w stanie zmienić zawartości strony", correct: false },
-        { text: "zawiera w kodzie dane, które są wyświetlane w przeglądarce", correct: false },
-        { text: "zawartość strony może zmienić się pod wpływem interakcji z użytkownikiem", correct: true },
-        { text: "każda zmiana danych wymaga ingerencji programisty w kod strony", correct: false }
+            { text: "użytkownik nie posiadający wiedzy na temat struktury strony nie jest w stanie zmienić zawartości strony", correct: false },
+            { text: "zawiera w kodzie dane, które są wyświetlane w przeglądarce", correct: false },
+            { text: "zawartość strony może zmienić się pod wpływem interakcji z użytkownikiem", correct: true },
+            { text: "każda zmiana danych wymaga ingerencji programisty w kod strony", correct: false }
         ]
     },
     {
         question: "Kod wykonywany po stronie serwera?",
         options: [
-        { text: "front-end", correct: false },
-        { text: "back-end", correct: true },
+            { text: "front-end", correct: false },
+            { text: "back-end", correct: true },
         ]
     },
     {
         question: "Znacznik - zaznacz fałszywe",
         options: [
-        { text: "tekst umieszczony pomiędzy znakiem większości i znakiem mniejszości", correct: false },
-        { text: "odnosi się do klasyfikowania informacji", correct: false },
-        { text: "klasyfikacja polega na umieszczeniu tekstu pomiędzy znacznikiem otwierającym i znacznikiem zamykającym", correct: false },
-        { text: "tekst umieszczony pomiędzy znakami #", correct: true }
+            { text: "tekst umieszczony pomiędzy znakiem większości i znakiem mniejszości", correct: false },
+            { text: "odnosi się do klasyfikowania informacji", correct: false },
+            { text: "klasyfikacja polega na umieszczeniu tekstu pomiędzy znacznikiem otwierającym i znacznikiem zamykającym", correct: false },
+            { text: "tekst umieszczony pomiędzy znakami #", correct: true }
         ]
     },
     {
         question: "Co nie jest tagiem w HTML?",
         options: [
-        { text: "<head>", correct: false },
-        { text: "<a>", correct: false },
-        { text: "<site> ", correct: true },
-        { text: "<div>", correct: false }
+            { text: "<head>", correct: false },
+            { text: "<a>", correct: false },
+            { text: "<site> ", correct: true },
+            { text: "<div>", correct: false }
         ]
     },
 ];
@@ -93,9 +93,9 @@ let currentQuestion = 0;
 let score = 0;
 let rightAnswer = false;
 
-function startQuiz(){
+function startQuiz() {
 
-    
+
 }
 
 function checkAnswer(option) {
@@ -112,7 +112,7 @@ function checkAnswer(option) {
     const options = document.querySelectorAll('.option');
     options.forEach(otherOption => {
         if (otherOption !== option) {
-            otherOption.classList.remove('clicked');  
+            otherOption.classList.remove('clicked');
         }
     });
     option.classList.add('clicked');
@@ -138,14 +138,18 @@ function nextQuestion() {
             options.appendChild(optionDiv);
         });
     } else {
-        if(score == questions.length) {
+        if (score == questions.length) {
             document.getElementById("result").classList.add("good");
         }
-        if(score == 0) {
+        if (score == 0) {
             document.getElementById("result").classList.add("bad");
         }
         document.getElementById("result").style.display = "block";
         document.getElementById("score").textContent = score;
         document.getElementById("nextButton").style.display = "none";
     }
+}
+
+function showAdminLogin() {
+    window.location.href = "admin.html";
 }
