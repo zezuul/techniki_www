5a71f407fcf37a89b2b769fa34351fb22bd22412
zezuul <zezula.julia@gmail.com>
2023-11-05 15:01:44 +0100

    Fixed admin button

--- quiz/js/login-page.js
+++ quiz/js/login-page.js
@@ -1,6 +1,8 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
+const adminPanelContainer = document.getElementById("admin-panel-container");
+const loginContainer = document.getElementById("admin-login-container");
 
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
@@ -9,7 +11,8 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "admin" && password === "admin1") {
         alert("Welcome to admin panel.");
-        // TODO wyswietl panel administratora
+        loginContainer.style.display = "none";
+        adminPanelContainer.style.display = "block";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
